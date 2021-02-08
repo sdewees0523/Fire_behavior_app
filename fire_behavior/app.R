04cfe8aec86bbfa3c37678f79af04b16befd54d9
sdewees0523 <sdewees@ucsb.edu>
2021-02-08 13:53:12 -0800

    Initial commit

---
+++ fire_behavior/app.R
@@ -0,0 +1,13 @@
+library(shiny)
+library(tidyverse)
+library(sf)
+library(raster)
+
+# Create the user interface
+ui <- fluidPage()
+
+# Create the server function
+server <- function(input, output) {}
+
+# Combine into an app
+shinyApp(ui = ui, server = server)
